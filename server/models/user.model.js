a40acfa4811f72b4227c3a206f35ce7df1444661
Amaresh Maharana <amareshcoder@gmail.com>
2025-08-11 21:22:18 +0530

    feat(user): add user model with authentication fields

---
+++ server/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      // trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    profilePicture: {
+      type: String,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    lastLogin: {
+      type: Date,
+    },
+    is2FAEnabled: {
+      type: Boolean,
+      default: false,
+    },
+    twoFAOtp: {
+      type: String,
+      select: false,
+    },
+    twoFAOtpExpires: {
+      type: Date,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
